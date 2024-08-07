b84c8f1d0676bed1aab396d535384fc9c851b06a
HenryLi-0 <bxsci8266@gmail.com>
2024-08-06 20:43:03 -0400

    motor stuff, reverted effects of time travel

--- src/main/java/org/sciborgs1155/robot/elevator/RealElevator.java
+++ src/main/java/org/sciborgs1155/robot/elevator/RealElevator.java
@@ -2,21 +2,29 @@ package org.sciborgs1155.robot.elevator;
 
 import static org.sciborgs1155.robot.Ports.Elevator.*;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 
 public class RealElevator implements ElevatorIO {
-  private final TalonFX lead;
-  private final TalonFX leftBottom;
-  private final TalonFX rightTop;
-  private final TalonFX rightBottom;
+  private final TalonFX lead  = new TalonFX(MOTOR_LEFT_TOP);
+  private final TalonFX leftBottom  = new TalonFX(MOTOR_LEFT_BOTTOM);
+  private final TalonFX rightTop  = new TalonFX(MOTOR_RIGHT_TOP);
+  private final TalonFX rightBottom  = new TalonFX(MOTOR_RIGHT_BOTTOM);
 
   public RealElevator() {
-    lead = new TalonFX(MOTOR_LEFT_TOP);
-    leftBottom = new TalonFX(MOTOR_LEFT_BOTTOM);
-    rightTop = new TalonFX(MOTOR_RIGHT_TOP);
-    rightBottom = new TalonFX(MOTOR_RIGHT_BOTTOM);
-
+    TalonFXConfiguration configuration = new TalonFXConfiguration();
     
+    lead.getConfigurator().apply(configuration);
+    leftBottom.getConfigurator().apply(configuration);
+    rightTop.getConfigurator().apply(configuration);
+    rightBottom.getConfigurator().apply(configuration);
+
+    leftBottom.setControl(new Follower(MOTOR_LEFT_TOP, true));
+    rightTop.setControl(new Follower(MOTOR_LEFT_TOP, false));
+    rightBottom.setControl(new Follower(MOTOR_LEFT_TOP, true));
+
   }
 
   @Override
